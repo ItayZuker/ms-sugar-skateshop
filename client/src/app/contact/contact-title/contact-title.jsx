f085506f6520db0788b0f58c6bc7c19142b6c988
Itay <zuker2085@gmail.com>
2023-12-14 08:04:35 +0200

    progress

--- client/src/app/contact/contact-title/contact-title.jsx
+++ client/src/app/contact/contact-title/contact-title.jsx
@@ -6,7 +6,7 @@ const ContactTitle = () => {
     /* JSX */
     return (
         <div className="contact-title-container">
-            <h4>contact@ms-sugar.com</h4>
+            <h3>Tell me everything:</h3>
         </div>
     )
 }
