2f67c69f745ee5fb31ee1929038703676745e2ed
Itay <zuker2085@gmail.com>
2024-02-11 08:03:48 +0200

    progress

--- client/src/app/exchange/e-form-section/e-form-main/e-form-message/e-form-message.jsx
+++ client/src/app/exchange/e-form-section/e-form-main/e-form-message/e-form-message.jsx
@@ -7,7 +7,7 @@ const EFormMessage = () => {
     /* JSX */
     return (
         <div className="e-form-message-container">
-            <p>Send a poem, a story, or any other form of text, in exchange for a 10 percent discount. You can choose to be anonymous, or you can give your name for credit. By sending your art, you agree that Ms-Sugar might use it for commercial use. Read more on <Link to="/terms-and-conditions" target="_blank">Terms and conditions.</Link></p>
+            <p>Send a poem, a story, or any other form of text, in exchange for a 10% discount. You can choose to be anonymous, or you can give your name for credit. By sending your art, you agree that Ms-Sugar might use it for commercial use. Read more on <Link to="/terms-and-conditions" target="_blank">Terms and conditions.</Link></p>
         </div>
     )
 }
