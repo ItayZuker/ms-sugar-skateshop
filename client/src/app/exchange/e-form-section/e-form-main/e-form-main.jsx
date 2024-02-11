a288b66ae42ee983555b6f75ede87a097df6b927
Itay <zuker2085@gmail.com>
2024-02-11 07:47:36 +0200

    progress

--- client/src/app/exchange/e-form-section/e-form-main/e-form-main.jsx
+++ client/src/app/exchange/e-form-section/e-form-main/e-form-main.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import EFormTitle from "./e-form-title/e-form-title"
 import EFormMessage from "./e-form-message/e-form-message"
 import EFormIAgree from "./e-form-i-agree/e-form-i-agree"
+import EFormSubmit from "./e-form-submit/e-form-submit"
 import "./e-form-main.scss"
 
 const EFormMain = () => {
@@ -11,8 +12,9 @@ const EFormMain = () => {
         <div className="e-form-main-container">
             <EFormTitle/>
             <EFormMessage/>
-            <EFormIAgree/>
+            {/* <EFormIAgree/> */}
             {/* // choose text direction| */}
+            <EFormSubmit/>
         </div>
     )
 }
---
+++ client/src/app/exchange/e-form-section/e-form-main/e-form-submit/e-form-submit.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./e-form-submit.scss"
+
+const EFormSubmit = () => {
+
+    /* JSX */
+    return (
+        <div className="e-form-submit-container">
+            <div className="submit">
+                <p>Send</p>
+            </div>
+        </div>
+    )
+}
+
+export default EFormSubmit
