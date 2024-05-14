b9788577e79d3bfcbdfa8779119bb38afa4f68df
Itay <zuker2085@gmail.com>
2024-05-14 19:11:16 +0300

    progress

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './app/app'
 import { GlobalProvider } from './context/global'
 import { ShopifyProvider } from './context/shopify'
-import registerServiceWorker from './lib/service-worker-setup'
+// import registerServiceWorker from './lib/service-worker-setup'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
@@ -15,4 +15,4 @@ root.render(
     </GlobalProvider>
 )
 
-registerServiceWorker()
+// registerServiceWorker()
